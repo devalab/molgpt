d9ff03f0091d6bd9e068c306589ffbe2c7a73c61
Viraj Bagal <virajbagal12@gmail.com>
2021-12-14 11:58:18 +0530

    Update train_guacamol.sh

--- train_guacamol.sh
+++ train_guacamol.sh
@@ -12,6 +12,6 @@ python train/train.py --run_name logp_sas_guacamol --data_name guacamol2 --batch
 python train/train.py --run_name scaffold_guacamol --data_name guacamol2 --scaffold --batch_size 384 --max_epochs 10
 
 # scaffold + property based conditional train/training
-python train/train.py --run_name logp_scaffold_guacamol --data_name guacamol2 --scaffold --batch_size 384 --num_props 1 --max_epochs 10 --property logp
+python train/train.py --run_name logp_scaffold_guacamol --data_name guacamol2 --scaffold --batch_size 384 --num_props 1 --max_epochs 10 --props logp
 
-python train/train.py --run_name logp_sas_scaffold_guacamol --data_name guacamol2 --scaffold --batch_size 384 --num_props 2 --max_epochs 10 --property logp sas
+python train/train.py --run_name logp_sas_scaffold_guacamol --data_name guacamol2 --scaffold --batch_size 384 --num_props 2 --max_epochs 10 --props logp sas
